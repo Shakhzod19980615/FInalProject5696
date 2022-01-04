d6d0b299578194a78b4a8090c985bca15a0dc0d4
Shakhzod19980615 <69627740+Shakhzod19980615@users.noreply.github.com>
2022-01-05 03:07:21 +0500

    Add files via upload

---
+++ Supporting files/ALLstrings.swift
@@ -0,0 +1,23 @@
+//
+//  Money Manager
+//
+//  Created by Ismoil Khojiakbarov on 8/7/20.
+//  Copyright © 2020 Ismoil Khojiakbarov. All rights reserved.
+//
+
+import Foundation
+
+class ALLstrings {
+    func uz() -> [String : String] {
+        let strArr = ["daromad_manbai": "Daromad manbai", "qanday_turdagi_daromodni_qoshmoqchisiz": "Qanday turdagi daromadni qo'shmoqchisiz ?", "oylik_maosh": "Oylik maosh", "biznes_daromad": "Biznes daromad", "boshqa_daromad": "Boshqa daromad", "daromad_turi : ": "Daromad turi : ", "qayerdan": "Qayerdan : ", "davom_etish": "Davom etish", "bekor_qilish": "Bekor qilish", "mablag_qoshish": "Mablag' qo'shish", "ozim": "O'zim", "ota_onam": "Ota - onam", "fond": "Fond", "oilam": "Oilam", "investitsiya": "Investitsiya", "qarz": "Qarz", "hisobot": "Hisobot", "xarajat": "Xarajat", "sana": "Sana", "kategoriya": "Kategoriya", "izoh": "Izoh", "mablag": "Mablag'", "saqlash": "Saqlash", "manba": "Manba", "tolangan": "To'langan  ✅", "tolash": "To'lash  ☑️", "daromad_manbalari": "Daromad manbalari", "barcha_malumotlarni_ochirish": "BARCHA MA'LUMOTLARNI O'CHIRISH", "ishonchingiz_komilmi": "Ishonchingiz komilmi ?", "ha": "Ha", "yoq": "Yoq"]
+        return strArr
+    }
+    func ru() -> [String : String] {
+        let strArr = ["daromad_manbai": "Источник дохода", "qanday_turdagi_daromodni_qoshmoqchisiz": "Какой тип дохода вы хотите добавить ?", "oylik_maosh": "Ежемесячная зарплата", "biznes_daromad": "Бизнес доход", "boshqa_daromad": "Прочие доходы", "daromad_turi : ": "Тип дохода : ", "qayerdan": "Откуда : ", "davom_etish": "Продолжай", "bekor_qilish": "Отмена", "mablag_qoshish": "Пополнить счет", "ozim": "Себя", "ota_onam": "Мои родители", "fond": "Фонд", "oilam": "Моя семья", "investitsiya": "Инвестиции", "qarz": "Долг", "hisobot": "Отчет", "xarajat": "Расход", "sana": "Дате", "kategoriya": "Категория", "izoh": "Заметка", "mablag": "Сумма", "saqlash": "Сохранить", "manba": "Источник", "tolangan": "Оплаченный  ✅", "tolash": "Оплата  ☑️", "daromad_manbalari": "Источники дохода", "barcha_malumotlarni_ochirish": "УДАЛИТЬ ВСЕ ДАННЫЕ", "ishonchingiz_komilmi": "Ты уверен ?", "ha": "Да", "yoq": "Нет"]
+        return strArr
+    }
+    func eng() -> [String : String] {
+        let strArr = ["daromad_manbai": "Source of income", "qanday_turdagi_daromodni_qoshmoqchisiz": "What type of income do you want to add?", "oylik_maosh": "Monthly salary", "biznes_daromad": "Business income", "boshqa_daromad": "Other income", "daromad_turi : ": "Income Type : ", "qayerdan": "Comment : ", "davom_etish": "Continue", "bekor_qilish": "Cancel", "mablag_qoshish": "Add funds", "ozim": "Myself", "ota_onam": "My parents", "fond": "Fund", "oilam": "My family", "investitsiya": "Investments", "qarz": "Debt", "hisobot": "Report", "xarajat": "Consumption", "sana": "Date", "kategoriya": "Category", "izoh": "Note", "mablag": "Funds", "saqlash": "Save", "manba": "Source", "tolangan": "Paid  ✅", "tolash": "Pay  ☑️", "daromad_manbalari": "Sources of income", "barcha_malumotlarni_ochirish": "REMOVE ALL DATA", "ishonchingiz_komilmi": "Are you sure ?", "ha": "Yes", "yoq": "No"]
+        return strArr
+    }
+}
